eb46ab758fdd6fc317d6a37e80a7c9607f62de4f
CrawFish666 <emelyakoro4@mail.ru>
2026-09-22 02:05:02 +0400

    feat: Доработать функциональность

--- src/components/SocialLink.jsx
+++ src/components/SocialLink.jsx
@@ -1,24 +1,48 @@
 import { Link } from "react-router-dom";
+import { iconMap, defaultIcon } from "../constants/iconMap";
 
 export function SocialLink({ item, customSize }) {
-	const Icon = item.iconComponent;
-	// const iconClassName = `w-${h} h-${h}`;
-	const baseClassName = "p-2.5 rounded-xl glass text-dark-400 hover:text-white transition-colors";
+	const Icon = iconMap[item.icon] ?? defaultIcon;
+
+	if (!iconMap[item.icon]) {
+		console.warn(
+			`Не найдена иконка "${item.icon}" для контакта "${item.label}"`
+		);
+	}
+	const baseClassName = "p-2.5 rounded-xl glass text-secondary hover:text-primary hover:scale-110 transition-colors";
 	const size = customSize ?? item.size ?? 20;
 
-	return (
-		item.isExternal ?
-			<a
-				href={item.path}
-				rel="noopener noreferrer"
-				target="_blank"
-				className={baseClassName}>
-				<Icon style={{ width: size, height: size }} />
-			</a> :
+	const getHref = () => {
+		switch (item.type) {
+			case "email":
+				return `mailto:${item.url}`;
+			case "tel":
+				return `tel:${item.url}`;
+		
+			default:
+				return item.url;
+		}}
+	const href = getHref();
+
+	// Внутренняя ссылка
+	if (!item.isExternal) {
+		return (
 			<Link
-				to={item.path}
-				className={baseClassName}>
+				to={href}
+				className={baseClassName}
+			>
 				<Icon style={{ width: size, height: size }} />
 			</Link>
+		);
+	}
+
+	return (
+		<a
+			href={href}
+			target={item.isExternal ? "_blank" : undefined}
+			rel={item.isExternal ? "noopener noreferrer" : undefined}
+			className={baseClassName}>
+			<Icon style={{ width: size, height: size }} />
+		</a>
 	)
 }
